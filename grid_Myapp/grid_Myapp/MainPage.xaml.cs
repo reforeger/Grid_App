eb5beebf5ca78d8950b9667ce5cbb47d84e5e25b
opilane <opilane@HP-CZC6297B8P>
2020-09-23 14:52:54 +0300

    new image krestiki noliki

--- grid_Myapp/grid_Myapp/MainPage.xaml.cs
+++ grid_Myapp/grid_Myapp/MainPage.xaml.cs
@@ -36,30 +36,53 @@ namespace grid_Myapp
                
             };*/
             Grid grid = new Grid();
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
                 grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
                 grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             }
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 3; i++)
             {
-                for (int j = 0; j < 5; j++)
+                for (int j = 0; j < 3; j++)
                 {
+                    Image img = new Image { Source = ImageSource.FromFile("nolik.png") };
                     box = new BoxView { Color = Color.FromRgb(200,100, 50) };
                     grid.Children.Add(box, i, j);
                     var tap = new TapGestureRecognizer();
-                    tap.Tapped += Tap_Tapped;
-                    box.GestureRecognizers.Add(tap);
+                    //tap.Tapped += Tap_Tapped;
+                    boxs[i,j].GestureRecognizers.Add(tap);
+                    tap.Tapped += async (object sender, EventArgs e) =>
+                        {/*
+                            BoxView box = sender as BoxView;
+                            if (box.Color == new Color(0, 0, 0))
+                            {
+                            box.Color = Color.FromRgb(200, 100, 50);
+                            }
+                            else
+                            {
+                                box.Color = new Color(0, 0, 0);
+                            }
+                            Image img1 = sender as image;
+                            if(img1.Source=="nolik.png")
+                            {
+                                img1.Source = "nuul.png";
+                            }
+                            else
+                            {
+                                img1.Source = "krest.png";
+                            }
+                            */
+                        };
                 }
             }
             Content = grid;
         }
 
-        private void Tap_Tapped(object sender, EventArgs e)
+       /* private void Tap_Tapped(object sender, EventArgs e)
         {
             BoxView box = sender as BoxView;
             box.Color = Color.FromRgb(0,0,0);
             
-        }
+        }*/
     }
 }
