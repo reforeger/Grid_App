5bc2957367186ee9cfb84da43b0d8f1f1ce14ea8
Opilane TTHK <opilane@tthk.ee>
2020-09-23 13:20:37 +0300

    Add project files.

---
+++ grid_Myapp/grid_Myapp/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace grid_Myapp
+{
+    public partial class MainPage : ContentPage
+    {
+        BoxView box;
+        public MainPage()
+        {
+            /*
+            {
+                RowDefinitions=
+                {
+                    
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)},
+                    new RowDefinition{Height=new GridLength(1,GridUnitType.Star)}
+                },
+                ColumnDefinitions=
+                {
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)},
+                    new ColumnDefinition{Width=new GridLength(1,GridUnitType.Star)}
+                }
+               
+            };*/
+            Grid grid = new Grid();
+            for (int i = 0; i < 5; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+                grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    box = new BoxView { Color = Color.FromRgb(200,100, 50) };
+                    grid.Children.Add(box, i, j);
+                    var tap = new TapGestureRecognizer();
+                    tap.Tapped += Tap_Tapped;
+                    box.GestureRecognizers.Add(tap);
+                }
+            }
+            Content = grid;
+        }
+
+        private void Tap_Tapped(object sender, EventArgs e)
+        {
+            BoxView box = sender as BoxView;
+            box.Color = Color.FromRgb(0,0,0);
+            
+        }
+    }
+}
